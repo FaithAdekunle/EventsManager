eade7fee7c9719cd4a3ee3cc8e959867a3d89863
Faith Adekunle <adegold71@gmail.com>
2017-11-24 05:42:36 +0100

    created api endpoint for modifying a center, also expanded the tests to cover more cases, adjusted controller to unmount images from local file in case an error occurs downthe line

--- server/controllers/centerController.js
+++ server/controllers/centerController.js
@@ -20,7 +20,13 @@ const options = {
     if(isImage) return next(null, true);
     return next({err: 'File type rejected' }, false);
   }
-}; 
+};
+
+const unmountImages = (obj) => {
+  for(let image of obj.images){
+    fs.unlinkSync(`./public/images/${image}`)
+  }
+} 
 
 const upload = multer(options);
 
@@ -32,7 +38,7 @@ export class CenterController {
       body("description").exists().isLength({ min: 1 }).trim(),
       body("facilities").exists().isLength({ min: 1 }).trim(),
       body("address").exists().isLength({ min: 1 }).trim(),
-      // body("images").exists(),
+      body("images").exists(),
       body("capacity").exists(),
       body("cost").exists(),
       sanitize("capacity").toInt(),
@@ -44,9 +50,19 @@ export class CenterController {
     return upload.array('images', 4);
   } 
 
+  static mountImages(req, res, next) {
+    req.body.images = [];
+    if(req.files) {
+      for(let file of req.files){
+        req.body.images.push(file.filename);
+      }
+    }
+    return next(); 
+  }
+
   static checkFailedValidations(req, res, next) {
-    if(validationResult(req).isEmpty()) return next();
-    console.log(validationResult(req).isEmpty());
+    if(validationResult(req).isEmpty() && Array.isArray(req.body.images) && req.body.images.length > 0) return next();
+    unmountImages(req.body);
     return res.json({ err: 'Incomplete details' });
   };
 
@@ -61,21 +77,39 @@ export class CenterController {
 
   static checkCostAndCapacityFields(req, res, next) {
     if(!Number.isInteger(req.body.cost) || !Number.isInteger(req.body.capacity) || [-1, 0, -0].includes(Math.sign(req.body.cost)) || [-1, 0, -0].includes(Math.sign(req.body.capacity))){
+      unmountImages(req.body);
       return res.json({ err: 'Invalid details. Only positive integers allowed' });
     }
     return next(); 
   };
 
   static addCenter(req, res, next){
-    req.body.images = [];
-    for(let file of req.files){
-      req.body.images.push(file.filename);
-    };
     fs.readFile('./data/centers.json', (err, data) => {
       if(err) return res.json({ err: 'local file database failure' });
       let centers = JSON.parse(data);
       const centerId = uuidv4();
       centers[centerId] = req.body;
+      let savedCenters = JSON.stringify(centers, null, 2);
+      fs.writeFile('./data/centers.json', savedCenters, (err) => {
+        if(err) return res.json({ err: 'local file database failure' });
+        req.body.id = centerId;
+        return res.json(req.body);
+      })
+    })
+  }
+
+  static modifyCenter(req, res, next){
+    fs.readFile('./data/centers.json', (err, data) => {
+      if(err) return res.json({ err: 'local file database failure' });
+      let centers = JSON.parse(data);
+      if(!centers[req.params.id]) {
+        unmountImages(req.body);
+        return  res.json({ err: 'center not found' });
+      }
+      const centerId = req.params.id;
+      const center = centers[centerId];
+      unmountImages(center);
+      centers[centerId] = req.body
       let savedCenters = JSON.stringify(centers, null, 2);
       fs.writeFile('./data/centers.json', savedCenters, (err) => {
         if(err) return res.json({ err: 'local file database failure' });
