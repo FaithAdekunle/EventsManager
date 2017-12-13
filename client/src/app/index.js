f66516879914cbe4d2e470ae22d063ff28fde267
Faith Adekunle <adegold71@gmail.com>
2017-12-13 19:06:50 +0100

    designed styled and rendered home page in the home component

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -4,6 +4,7 @@ import 'font-awesome/scss/font-awesome.scss';
 import 'font-awesome-loader';
 import React from 'react';
 import { render } from 'react-dom';
+import './index.sass';
 import AppRouter from './components/AppRouterComponent';
 
 render(<AppRouter />, window.document.getElementById('container'));
