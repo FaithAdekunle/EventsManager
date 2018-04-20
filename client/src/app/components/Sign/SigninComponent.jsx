7910bfe56b87377a972d4f070258e7bb152672df
Faith Adekunle <adegold71@gmail.com>
2018-04-20 11:50:18 +0100

    feat(login): -refactored login process on SigninComponent.jsx and DialApi.js files [Finishes #152805236]

--- client/src/app/components/Sign/SigninComponent.jsx
+++ client/src/app/components/Sign/SigninComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import OtherActions from '../../actions/otherActions';
+import DialApi from '../../DialApi';
 
 /**
  * SignIn component class
@@ -20,6 +21,7 @@ class SignIn extends React.Component {
     this.changeFormState = this.changeFormState.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.navToSignup = this.navToSignup.bind(this);
+    this.beforeLogin = this.beforeLogin.bind(this);
     this.onUserLoginFail = this.onUserLoginFail.bind(this);
     this.onUserLoginSuccessful = this.onUserLoginSuccessful.bind(this);
   }
@@ -29,10 +31,6 @@ class SignIn extends React.Component {
    * @returns { void }
    */
   componentWillUnmount() {
-    OtherActions.updatePageState({
-      userOnSignInPage: false,
-      userOnSignUpPage: false,
-    });
     OtherActions.updateAlertState(null);
   }
 
@@ -47,19 +45,10 @@ class SignIn extends React.Component {
 
   /**
    * executes after failed login attempt
-   * @param { object } response
    * @returns { void }
    */
-  onUserLoginFail(response) {
+  onUserLoginFail() {
     this.changeFormState(false);
-    if (!response) {
-      return OtherActions
-        .updateAlertState(`Looks like you're offline. 
-        Check internet connection.`);
-    }
-    OtherActions.updateAlertState(Array.isArray(response.data.error) ?
-      response.data.error[0] : response.data.error);
-    return setTimeout(() => OtherActions.updateAlertState(null), 10000);
   }
 
   /**
@@ -69,13 +58,25 @@ class SignIn extends React.Component {
    */
   onSubmit(event) {
     event.preventDefault();
-    this.changeFormState();
     const credentials = {
       email: this.email.value,
       password: this.password.value,
     };
-    OtherActions
-      .login(credentials, this.onUserLoginSuccessful, this.onUserLoginFail);
+    DialApi
+      .login(
+        this.beforeLogin,
+        credentials,
+        this.onUserLoginSuccessful,
+        this.onUserLoginFail,
+      );
+  }
+
+  /**
+   * executes before signup
+   * @returns { void }
+   */
+  beforeLogin() {
+    this.changeFormState();
   }
 
   /**
